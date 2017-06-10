456d14e7dbe022440cb3df55d79eb15004725bba
huangwei <huangwei@huangweideiMac.local>
2017-06-10 17:54:56 +0800

    update

--- www/static/js/home/index_generator_iframe.js
+++ www/static/js/home/index_generator_iframe.js
@@ -1,5 +1,34 @@
 window.onload = function() {
 
+  class Communicator {
+    constructor(target = window.top.Communicator) {
+      // init props
+      this.target = target
+      this.listeners = {}
+    }
+    // 接受消息
+    receive(type, ...opts) {
+      const listeners = this.listeners[type]
+      if(listeners) {
+        listeners.forEach(cb => {
+          cb()
+        })
+      }
+    }
+
+    // 发送消息
+    send(type, opts) {
+      this.target.receive(type, opts)
+    }
+
+    on(type, cb) {
+      this.listeners[type] = this.listeners[type] || []
+      this.listeners[type].push(cb)
+    }
+  }
+
+  // 与外部通信
+
   let Parser = {
     // 选择目标类型：image, title, prize, link
     selectType: 'image',
